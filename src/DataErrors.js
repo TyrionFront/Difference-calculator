e892bc2e39c9b8770791cb6c137077d94c258db7
TyrionFront <GingerTormund@protonmail.com>
2018-10-11 19:32:15 +0300

    add custom errors object

---
+++ src/DataErrors.js
@@ -0,0 +1,15 @@
+export default class extends Error {
+  constructor(type) {
+    super(`${type}`);
+
+    this.name = 'DataTypeError';
+    this.type = type;
+    this.message = `${type} is incorrect data type. These data can not be parsed.
+      Please, try again with data of ".JSON", ".yaml" or ".ini" types\n`;
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = (new Error()).stack;
+    }
+  }
+}
