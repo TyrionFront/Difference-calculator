399317282768a3d90ce463f84351337b78c27745
TyrionFront <GingerTormund@protonmail.com>
2018-10-13 18:40:07 +0300

    refactor json

--- src/renderers/json.js
+++ src/renderers/json.js
@@ -1,11 +1,1 @@
-const render = data => data.map((node) => {
-  const { type, name, content } = node;
-  if (type === 'nested') { return { type, name, content: render(content) }; }
-  if (type === 'added') {
-    const [, newValue] = content;
-    return { type, name, content: [newValue] };
-  }
-  return { type, name, content };
-});
-
-export default ast => JSON.stringify(render(ast), null, 2);
+export default ast => JSON.stringify(ast, null, 2);
